f487d82d717bb7fadd577bc4f5753e06ce58ef2d
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-01 07:02:48 -0300

    banner configurado

--- src/componentes/banner/banner.js
+++ src/componentes/banner/banner.js
@@ -1,7 +1,11 @@
+import './banner.css'
+
 function Banner() {
     // Não é HTML, é JSX
     return (
+        <header className='banner'>
         <img src="/imagens/banner.png"alt="Banner principal da página do Organo"/>
+        </header>
     )
 }
 
