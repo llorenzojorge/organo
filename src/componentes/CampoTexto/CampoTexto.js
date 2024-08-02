8dbccae68b4e4e52876aa27460cf6203aff36c5a
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-02 05:47:06 -0300

    colocando html e css

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,7 +1,18 @@
+import './CampoTexto.css'
+
 const CampoTexto = () => {
     return (
-        <div>
+        <div className="campo-texto">
             <label>Nome</label>
+            <input placeholder='Digite seu nome'/>
+
+            <label>Cargo</label>
+            <input placeholder='Digite seu cargo'/>
+
+            <label>Imagem</label>
+            <input placeholder='Informe o endereço de imagem'/>
+
+            <label>Time</label>
             <input />
         </div>
     )
