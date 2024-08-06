4349d89db31088b67e6da63a59e42e9f021d23fe
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-06 11:17:44 -0300

    alterando css

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -3,11 +3,12 @@ import './Formulario.css'
 
 const Formulario = () => {
     return (
-        <section>
+        <section className='formulario'>
             <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTextoexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
             </form>
         </section>
     )
