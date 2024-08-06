13d070ef332df5d147027c4b624377fb55076b04
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-06 10:47:59 -0300

    Update index.js

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,13 @@
 import './Formulario.css'
 
 const Formulario = () => {
-
+    return (
+        <section>
+            <form>
+                
+            </form>
+        </section>
+    )
 }
 
 export default Formulario
