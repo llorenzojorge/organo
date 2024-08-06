f90cace3a82f82c030882911493916e7e16a5d3c
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-06 10:50:06 -0300

    Coloquei o CampoTexto dentro do Formulario

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,10 +1,13 @@
+import CampoTexto from '../CampoTexto/CampoTexto'
 import './Formulario.css'
 
 const Formulario = () => {
     return (
         <section>
             <form>
-                
+                <CampoTexto label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTextoexto label="Imagem" placeholder="Digite o endereço da imagem" />
             </form>
         </section>
     )
