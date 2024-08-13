0a9ffc0e6b94761baea8fbcc04b1c1babd01fafc
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2024-08-13 15:40:19 -0300

    Update index.js

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,7 +5,9 @@ const ListaSuspensa = (props) => {
         <div>
             <label>{props.label}</label>
             <select>
-                {props.itens.map(item => <option key={item}>{item}</option>)}
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
             </select>
         </div>
     )
